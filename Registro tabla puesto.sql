d4dfe3e11a88926654109546e98b7658c3cb1942
EleanFPC <eleanperez10@gmail.com>
2023-05-26 11:53:48 -0600

    agregue estos registros

---
+++ Registro tabla puesto.sql
@@ -0,0 +1,61 @@
+Insert Into Puesto (Codigo ,Descripcion, Activo)
+VALUES (001, 'Gerente', 1);
+
+Insert Into Puesto (Codigo ,Descripcion, Activo)
+VALUES (002, 'Analista', 0);
+
+Insert Into Puesto (Codigo ,Descripcion, Activo)
+VALUES (003, 'Asistente', 1);
+
+Insert Into Puesto (Codigo ,Descripcion, Activo)
+VALUES (004, 'Supervisor', 1);
+
+Insert Into Puesto (Codigo ,Descripcion, Activo)
+VALUES (005, 'Tecnico', 0);
+
+Insert Into Puesto (Codigo ,Descripcion, Activo)
+VALUES (006, 'Ejecutivo de ventas', 1);
+
+Insert Into Puesto (Codigo ,Descripcion, Activo)
+VALUES (007, 'Desarrollador', 1);
+
+Insert Into Puesto (Codigo ,Descripcion, Activo)
+VALUES (008, 'Contador', 1);
+
+Insert Into Puesto (Codigo ,Descripcion, Activo)
+Values (009, 'operario', 1);
+
+INSERT INTO Puesto (Codigo, Descripcion, Activo)
+VALUES (010, 'Recepcionista', 1);
+
+INSERT INTO Puesto (Codigo, Descripcion, Activo)
+VALUES (011, 'Vendedor', 1);
+
+INSERT INTO Puesto (Codigo, Descripcion, Activo)
+VALUES (012, 'Ingeniero', 1);
+
+INSERT INTO Puesto (Codigo, Descripcion, Activo)
+VALUES (013, 'Diseñador', 1);
+
+INSERT INTO Puesto (Codigo, Descripcion, Activo)
+VALUES (014, 'Consultor', 1);
+
+INSERT INTO Puesto (Codigo, Descripcion, Activo)
+VALUES (015, 'Coordinador', 1);
+
+INSERT INTO Puesto (Codigo, Descripcion, Activo)
+VALUES (016, 'Ejecutivo de Ventas', 1);
+
+INSERT INTO Puesto (Codigo, Descripcion, Activo)
+VALUES (017, 'Asistente Administrativo', 1);
+
+INSERT INTO Puesto (Codigo, Descripcion, Activo)
+VALUES (018, 'Instructor', 1);
+
+INSERT INTO Puesto (Codigo, Descripcion, Activo)
+VALUES (019, 'Analista de Sistemas', 1);
+
+INSERT INTO Puesto (Codigo, Descripcion, Activo)
+VALUES (020, 'Recursos Humanos', 1);
+
+select * from[dbo].[Puesto]
---
+++ Registro tabla reclutador.sql
@@ -0,0 +1,4 @@
+Insert Into Reclutador (Id, Identificacion, Nombre, Apellido, Email, Telefono, Direccion, Activo, EsReclutador, EsEntrevistador)
+VALUES (1, 1234567890, 'Juan Jose', 'Perez Castillo', 'juan@example.com', 555-123-4567, 'Calle Principal, #123', 1, 1, 0);
+
+Select * from[dbo].[Reclutador]
